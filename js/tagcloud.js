b28d119ac51fe4c2610fc659cf5f62f4cd7c9149
FFFamily <tjj747487547@sina.com>
2020-01-04 19:01:14 +0800

    Site updated: 2020-01-04 19:01:11

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -12,10 +12,10 @@ addLoadEvent(function() {
    console.log('tag cloud plugin rock and roll!');
    try {
        TagCanvas.textFont = 'Trebuchet MS, Helvetica';
-       TagCanvas.textColour = '#333';
+       TagCanvas.textColour = '#00FFFF';
        TagCanvas.textHeight = 25;
        TagCanvas.outlineColour = '#E2E1D1';
-       TagCanvas.maxSpeed = 0.5;
+       TagCanvas.maxSpeed = 0.7;
        TagCanvas.outlineMethod = 'block';
        TagCanvas.minBrightness = 0.2;
        TagCanvas.depth = 0.92;
